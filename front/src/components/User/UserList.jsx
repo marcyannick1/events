41ebe88a21898f78ee9c46444c2219c632739cc7
Jokast38 <115721772+Jokast38@users.noreply.github.com>
2025-04-11 13:58:38 +0200

    update UserList component to allow full-width display by changing maxWidth style

--- front/src/components/User/UserList.jsx
+++ front/src/components/User/UserList.jsx
@@ -28,7 +28,7 @@ const styles = {
         padding: '20px',
         borderRadius: '8px',
         boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)',
-        maxWidth: '600px',
+        maxWidth:' 100%',
         margin: '20px auto',
         fontFamily: 'Arial, sans-serif',
     },
